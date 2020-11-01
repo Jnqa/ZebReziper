11d1fa68bc16bf07df4eaf80d1df30b7daace3b2
Jnqa <39852526+Jnqa@users.noreply.github.com>
2020-11-01 16:03:15 +0300

    Tesh with sh

---
+++ reziper.sh
@@ -0,0 +1,7 @@
+sudo apt-get install unzip -y zip;
+unzip $1 -d rezip;
+find . -name "*.debug" -exec rm {} \;
+find . -name "*.sym" -exec mv {} *.so;
+find . -name '*.sym' -exec sh -c 'mv "$0" "${0%.sym}.so"' {} \;
+cd rezip &&  zip -9 -r $1 * && mv $1 ../$1 && rm -r ../rezip;
+echo 'Reziped!'
